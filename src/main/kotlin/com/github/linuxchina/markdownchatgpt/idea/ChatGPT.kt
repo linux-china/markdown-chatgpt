e762f85148faaf8980fe221b8dc61fc2ad68b751
Libing Chen <libing.chen@gmail.com>
2023-05-03 15:42:26 +0800

    feat: add OpenAI icon

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
@@ -3,3 +3,4 @@ package com.github.linuxchina.markdownchatgpt.idea
 import com.intellij.openapi.util.IconLoader
 
 val chatGPTIcon = IconLoader.getIcon("icons/chatgpt.svg", MarkdownGPTFileIconProvider::class.java)
+val openAIIcon = IconLoader.getIcon("icons/openai-16.png", MarkdownGPTFileIconProvider::class.java)
