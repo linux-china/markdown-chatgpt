d925a1dfcdf2afbbdd3214f2007463e4da208ed0
GitHub Action <action@github.com>
2023-05-03 05:48:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.linuxchina.markdownchatgpt.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/services/MyProjectService.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.linuxchina.markdownchatgpt.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linuxchina.markdownchatgpt.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
