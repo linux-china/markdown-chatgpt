0060ead0a20e50ac339370063d87a0060674ba0e
Libing Chen <libing.chen@gmail.com>
2023-05-03 15:07:21 +0800

    test: polishing

--- src/test/kotlin/com/github/linuxchina/markdownchatgpt/MyPluginTest.kt
+++ src/test/kotlin/com/github/linuxchina/markdownchatgpt/MyPluginTest.kt
@@ -1,12 +1,10 @@
 package com.github.linuxchina.markdownchatgpt
 
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import com.github.linuxchina.markdownchatgpt.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -29,11 +27,6 @@ class MyPluginTest : BasePlatformTestCase() {
         myFixture.testRename("foo.xml", "foo_after.xml", "a2")
     }
 
-    fun testProjectService() {
-        val projectService = project.service<MyProjectService>()
-
-        assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
-    }
 
     override fun getTestDataPath() = "src/test/testData/rename"
 }
