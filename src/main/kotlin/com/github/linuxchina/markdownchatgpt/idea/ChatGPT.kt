df6568a14d736106abde9a00e933014dfcc700f1
Libing Chen <libing.chen@gmail.com>
2023-05-03 14:51:02 +0800

    app: ChatGPT

---
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
@@ -0,0 +1,5 @@
+package com.github.linuxchina.markdownchatgpt.idea
+
+import com.intellij.openapi.util.IconLoader
+
+val chatGPTIcon = IconLoader.getIcon("icons/chatgpt.svg", MarkdownGPTFileIconProvider::class.java)
