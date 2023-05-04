f2c1d28d0e0fff81ecd85d1f957e14e050aaf671
Libing Chen <libing.chen@gmail.com>
2023-05-04 18:11:56 +0800

    feat: add content replace

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/run/ChatGPTRequestMarkerProvider.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/run/ChatGPTRequestMarkerProvider.kt
@@ -1,14 +1,17 @@
 package com.github.linuxchina.markdownchatgpt.idea.run
 
+import com.github.linuxchina.markdownchatgpt.idea.ChatGPTDocument
 import com.github.linuxchina.markdownchatgpt.idea.chatGPTIcon
+import com.github.linuxchina.markdownchatgpt.idea.chatGPTResponseMarker
 import com.github.linuxchina.markdownchatgpt.idea.openAIIcon
 import com.intellij.codeInsight.daemon.LineMarkerInfo
-import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.lineMarker.RunLineMarkerProvider
-import com.intellij.ide.actions.runAnything.activity.RunAnythingCommandProvider
-import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownHeader
 import javax.swing.Icon
@@ -36,7 +39,7 @@ class ChatGPTRequestMarkerProvider : RunLineMarkerProvider() {
                             "Talk with ChatGPT"
                         },
                         { e, elt ->
-                            runDxTaskByRunAnything(psiElement.project, psiElement)
+                            updateChatGPTResponse(psiElement.project, psiElement)
                         },
                         GutterIconRenderer.Alignment.CENTER,
                         { "Talk with ChatGPT" }
@@ -59,12 +62,43 @@ class ChatGPTRequestMarkerProvider : RunLineMarkerProvider() {
         return null
     }
 
-    private fun runDxTaskByRunAnything(project: Project, psiElement: MarkdownHeader) {
-        RunAnythingCommandProvider.runCommand(
-            psiElement.containingFile.virtualFile.parent,
-            "dx xxx",
-            DefaultRunExecutor.getRunExecutorInstance(),
-            SimpleDataContext.getProjectContext(project)
-        )
+    private fun updateChatGPTResponse(project: Project, psiElement: MarkdownHeader) {
+        val chatGPTDocument = ChatGPTDocument(psiElement.parent)
+        val request = chatGPTDocument.findRequest(psiElement)!!
+        ApplicationManager.getApplication()
+            .runWriteAction {
+                val documentManager = PsiDocumentManager.getInstance(project)
+                val document = documentManager.getDocument(psiElement.containingFile)!!
+                var caretOffset = 0
+                val chatGPTReply = "\n\nChatGPT response\n\n"
+                if (request.responseContentOffset > 0) { // `#### ChatGPT` found
+                    val startOffset =
+                        psiElement.textRange.startOffset + psiElement.text.length + request.responseContentOffset
+                    val endOffset =
+                        psiElement.textRange.startOffset + psiElement.text.length + request.responseContentEndOffset
+                    document.replaceString(
+                        startOffset,
+                        endOffset,
+                        chatGPTReply
+                    )
+                    caretOffset = startOffset
+                } else {
+                    val startOffset =
+                        psiElement.textRange.startOffset + psiElement.text.length + request.responseContentEndOffset
+                    val endOffset =
+                        psiElement.textRange.startOffset + psiElement.text.length + request.responseContentEndOffset
+                    document.replaceString(
+                        startOffset,
+                        endOffset,
+                        "${chatGPTResponseMarker}${chatGPTReply}"
+                    )
+                    caretOffset = startOffset + chatGPTResponseMarker.length + 1
+                }
+                FileEditorManager.getInstance(project).selectedEditor?.let {
+                    if (it is TextEditor) {
+                        it.editor.caretModel.moveToOffset(caretOffset + 1)
+                    }
+                }
+            }
     }
 }
