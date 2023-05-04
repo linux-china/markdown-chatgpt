3739441b8dfa6c3834aa746c42cfac3d2ac67432
Libing Chen <libing.chen@gmail.com>
2023-05-04 16:56:43 +0800

    feat: add convertBodyToMessages()

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPTDocument.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPTDocument.kt
@@ -9,6 +9,37 @@ class ChatGPTRequest {
     var mdText: String? = null
     var response: String? = null
     var source: PsiElement? = null
+
+     fun convertBodyToMessages(): List<Map<String, Any>> {
+        val messages = mutableListOf<Map<String, Any>>()
+        var userMsgContent = mdText!!
+        //system message
+        val systemMsgPattern = Regex("(\\S.+\\n)*.+\\{\\.system}")
+        systemMsgPattern.find(userMsgContent)?.let {
+            it.groups[0]?.let { group ->
+                val matchedText = group.value
+                userMsgContent = userMsgContent.replace(matchedText, "").trim()
+                val systemMsgContent = matchedText.replace("{.system}", "").trim()
+                messages.add(mapOf("role" to "system", "content" to systemMsgContent))
+            }
+        }
+        //assistant messages
+        val assistantMessages = mutableListOf<Map<String, Any>>()
+        val assistantMsgPattern = Regex("(\\S.+\\n)*.+\\{\\.assistant}")
+        assistantMsgPattern.findAll(userMsgContent).forEach {
+            val matchedText = it.value
+            userMsgContent = userMsgContent.replace(matchedText, "").trim()
+            val assistantMsgContent = matchedText.replace("{.assistant}", "").trim()
+            assistantMessages.add(mapOf("role" to "assistant", "content" to assistantMsgContent))
+        }
+        // user message
+        messages.add(mapOf("role" to "user", "content" to userMsgContent))
+        // append assistant messages
+        if (assistantMessages.isNotEmpty()) {
+            messages.addAll(assistantMessages)
+        }
+        return messages
+    }
 }
 
 class ChatGPTDocument(val root: PsiElement) {
