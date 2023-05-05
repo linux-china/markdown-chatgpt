89f3099881278cc4c67624d89822c9af49c9e024
Libing Chen <libing.chen@gmail.com>
2023-05-05 09:16:36 +0800

    feat: add sendRequestIcon

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
@@ -1,7 +1,9 @@
 package com.github.linuxchina.markdownchatgpt.idea
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 
+val sendRequestIcon = AllIcons.RunConfigurations.TestState.Run
 val chatGPTIcon = IconLoader.getIcon("icons/chatgpt.svg", MarkdownGPTFileIconProvider::class.java)
 val openAIIcon = IconLoader.getIcon("icons/openai-16.png", MarkdownGPTFileIconProvider::class.java)
 
