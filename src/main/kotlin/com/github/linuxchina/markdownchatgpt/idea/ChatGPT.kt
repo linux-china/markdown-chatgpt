8ca05a5e6603e12d9f73c8685871eea0c95d8717
Libing Chen <libing.chen@gmail.com>
2023-05-06 10:06:06 +0800

    chore: use RunnableMark icon

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
@@ -3,7 +3,7 @@ package com.github.linuxchina.markdownchatgpt.idea
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 
-val sendRequestIcon = AllIcons.RunConfigurations.TestState.Run
+val sendRequestIcon = AllIcons.Nodes.RunnableMark
 val chatGPTIcon = IconLoader.getIcon("icons/chatgpt.svg", MarkdownGPTFileIconProvider::class.java)
 val openAIIcon = IconLoader.getIcon("icons/openai-16.png", MarkdownGPTFileIconProvider::class.java)
 
