986a6583d2791cc4f2e4d8196866f44493fa3b64
Libing Chen <libing.chen@gmail.com>
2023-06-15 19:11:17 +0800

    feat: add chatGPTFunctionsIcon

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
@@ -6,5 +6,6 @@ import com.intellij.openapi.util.IconLoader
 val sendRequestIcon = AllIcons.Nodes.RunnableMark
 val chatGPTIcon = IconLoader.getIcon("icons/chatgpt.svg", MarkdownGPTFileIconProvider::class.java)
 val openAIIcon = IconLoader.getIcon("icons/openai-16.png", MarkdownGPTFileIconProvider::class.java)
+val chatGPTFunctionsIcon = AllIcons.Nodes.Function
 
 const val chatGPTResponseMarker = "\n##### ChatGPT"
