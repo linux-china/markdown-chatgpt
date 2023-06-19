88e6e63363fa1ccd4fe093e2270c512d1fe5336b
Libing Chen <libing.chen@gmail.com>
2023-06-19 10:15:34 +0800

    chore: adjust sendRequestIcon

--- src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
+++ src/main/kotlin/com/github/linuxchina/markdownchatgpt/idea/ChatGPT.kt
@@ -3,7 +3,7 @@ package com.github.linuxchina.markdownchatgpt.idea
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.IconLoader
 
-val sendRequestIcon = AllIcons.Nodes.RunnableMark
+val sendRequestIcon = IconLoader.getIcon("icons/run.svg", MarkdownGPTFileIconProvider::class.java)
 val chatGPTIcon = IconLoader.getIcon("icons/chatgpt.svg", MarkdownGPTFileIconProvider::class.java)
 val openAIIcon = IconLoader.getIcon("icons/openai-16.png", MarkdownGPTFileIconProvider::class.java)
 val chatGPTFunctionsIcon = AllIcons.Nodes.Function
